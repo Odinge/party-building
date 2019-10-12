1f2e40315af38d7d740a86d2b916d51a7cf8d9ca
Odinge <2539625765@qq.com>
2019-10-12 15:34:53 +0800

    update

--- public/js/web.config.js
+++ public/js/web.config.js
@@ -7,8 +7,8 @@
  */
 module.exports = {
   baseURL: "/api",
-  webURL: "http://117.50.73.238:8080",
-  // webURL: "http://113.54.11.44:8080",
+  // webURL: "http://117.50.73.238:8080",
+  webURL: "http://113.54.11.44:8080",
   webDevURL: "http://117.50.73.238:8080",
   webProURL: "http://113.54.11.44:8080"
 };
